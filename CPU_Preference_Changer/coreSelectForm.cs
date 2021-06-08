bb8af99ad7c1f885d964bdfe997519def684717c
Edelsheim <skyline7651@gmail.com>
2021-06-08 23:55:28 +0900

    upload fixed

--- CPU_Preference_Changer/coreSelectForm.cs
+++ CPU_Preference_Changer/coreSelectForm.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CPU_Preference_Changer {
     public partial class coreSelectForm : Form {
         private IntPtr selCoreState = IntPtr.Zero;
 
-        public coreSelectForm(int maxCoreCnt,ulong curState)
+        public coreSelectForm(int maxCoreCnt, ulong curState)
         {
             InitializeComponent();
             initChkListBox(maxCoreCnt, curState);
--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # Mabi_CPU_Preference_Changer
 
-마비노기 다클라 Helper
+마비노기 클라이언트를 CPU 분배로 다클라 성능 향상
 
-배포 : https://cafe.naver.com/mabinogidsg/642329
+refer : <https://cafe.naver.com/mabinogidsg/642329>
 
-개발 환경 : Visual Studio 2019 , WPF , Win32 API
+## Build & Test Environment
 
-.Net Framework : 4.6
+```Text
+OS : Windows 10 Pro 64bit 20H2
+
+Build : Visual Studio 2019
+
+.Net Framework : 4.0
+```
+
+## Change Log
+
+```Text
+
+2021-06-08
+    - add trayicon
+    - set activity window
+    - memory GC
+    - auto refresh process list
+    - check same process is running
+```
